b90efe59e8e605892c1fcf714f5e72a03fbfb1e7
Ahmedbadr22 <ahmedbadr200121@gmail.com>
2023-05-03 23:18:02 +0300

    change db from mysql to sqlite

--- medita_api/settings.py
+++ medita_api/settings.py
@@ -101,14 +101,21 @@ WSGI_APPLICATION = 'medita_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'medifklh_medita_db',
+#         'USER': 'medifklh_root',
+#         'PASSWORD': 'm8iSt4WjZsk8DCP',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'medifklh_medita_db',
-        'USER': 'medifklh_root',
-        'PASSWORD': 'm8iSt4WjZsk8DCP',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
